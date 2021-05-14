be044345ca4e719fbaa0205777f2b72b1a9d2ee0
sky-leaf <30424804+sky-leaf@users.noreply.github.com>
2021-05-14 15:17:58 +0800

    Update puppeteer.js
    Fix: 优化puppeteer的加载速度

--- puppeteer.js
+++ puppeteer.js
@@ -1,6 +1,6 @@
-const puppeteer = require('puppeteer')
-const fs = require('fs')
-const _async = require('async')
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const _async = require('async');
 const {
   red,
   blue,
@@ -8,36 +8,79 @@ const {
   spider,
   getRandomInt,
   shuffleSort,
-  log,
-} = require('./utils')
-const { categoryTotalInfo } = require('./Home_Kitchen')
-const Goods = require('./dbs/model/goods')
+  log
+} = require('./utils');
+const { categoryTotalInfo } = require('./Home_Kitchen');
+const Goods = require('./dbs/model/goods');
 const browserOption = {
   headless: true,
   // 延迟
-  slowMo: 5,
-}
+  // slowMo: 5,
+  args: [
+    '–disable-gpu', // GPU硬件加速
+    '–disable-dev-shm-usage', // 创建临时文件共享内存
+    '–disable-setuid-sandbox', // uid沙盒
+    '–no-first-run', // 没有设置首页。在启动的时候，就会打开一个空白页面。
+    '–no-sandbox', // 沙盒模式
+    '--disable-accelerated-2d-canvas', // canvas渲染
+    '–no-zygote',
+    '–single-process' // 单进程运行
+  ]
+};
 let userAgent = [
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-]
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+];
 const chromeOption = {
   viewport: {
     width: 1280,
-    height: 800,
+    height: 800
   },
-  userAgent: userAgent[getRandomInt({ max: 1 })],
-}
+  userAgent: userAgent[getRandomInt({ max: 1 })]
+};
 const waitGotoUrlOption = {
   waitUntil: 'domcontentloaded',
-  timeout: 0,
-}
+  timeout: 0
+};
+
+const blockedResourceTypes = [
+  'image',
+  'media',
+  'font',
+  'texttrack',
+  'object',
+  'beacon',
+  'csp_report',
+  'imageset'
+];
+
+const skippedResources = [
+  'quantserve',
+  'adzerk',
+  'doubleclick',
+  'adition',
+  'exelator',
+  'sharethrough',
+  'cdn.api.twitter',
+  'google-analytics',
+  'googletagmanager',
+  'google',
+  'fontawesome',
+  'facebook',
+  'analytics',
+  'optimizely',
+  'clicktale',
+  'mixpanel',
+  'zedo',
+  'clicksor',
+  'tiqcdn'
+];
 
 async function getProductsInfo(page, productDetailInfo) {
   try {
     const productInfo = await page.evaluate(() => {
-      let list = []
-      document.querySelectorAll('.zg-item-immersion').forEach(item => {
+      let list = [];
+      document.querySelectorAll('.zg-item-immersion').forEach((item) => {
         if (item.querySelector('.a-link-normal')) {
           list.push({
             link: item.querySelector('.a-link-normal')?.href,
@@ -49,15 +92,15 @@ async function getProductsInfo(page, productDetailInfo) {
             )?.innerText,
             picture: item.querySelector('.a-section.a-spacing-small img')?.src,
             price:
-              item.querySelector('.a-size-base.a-color-price')?.innerText || 0,
-          })
+              item.querySelector('.a-size-base.a-color-price')?.innerText || 0
+          });
         }
-      })
-      return list
-    })
-    productDetailInfo.push(...productInfo)
+      });
+      return list;
+    });
+    productDetailInfo.push(...productInfo);
   } catch (err) {
-    red(err, 'error')
+    red(err, 'error');
   }
 }
 
@@ -66,24 +109,31 @@ async function gotoNextPage(page, productDetailInfo) {
     while (await page.$('.a-pagination .a-last a')) {
       const nextPage = await page.$eval(
         '.a-pagination .a-last a',
-        item => item.href
-      )
-      await page.goto(nextPage, waitGotoUrlOption)
-      await page.waitForTimeout(getRandomInt({ min: 1000, max: 1500 }))
-      await getProductsInfo(page, productDetailInfo)
+        (item) => item.href
+      );
+      await page.goto(nextPage, waitGotoUrlOption);
+      // await page.waitForTimeout(getRandomInt({ min: 1000, max: 1500 }))
+      await getProductsInfo(page, productDetailInfo);
     }
   } catch (err) {
-    red(err, 'error')
+    log(red(err, 'error'));
   }
 }
 
 async function run(url, type, productDetailInfo, callback) {
-  const browser = await puppeteer.launch(browserOption)
+  const browser = await puppeteer.launch(browserOption);
   try {
-    const page = await browser.newPage()
-    await spider(page)
-    await page.emulate(chromeOption)
-    await page.goto(url, waitGotoUrlOption)
+    const page = await browser.newPage();
+    await spider(page);
+    await page.emulate(chromeOption);
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+      if ([...blockedResourceTypes, ...skippedResources].includes(req.resourceType())) {
+        return req.abort();
+      }
+      return req.continue();
+    });
+    await page.goto(url, waitGotoUrlOption);
     // 设置  纽约
     // await page.waitForSelector(
     //   '.glow-toaster-button-submit .a-button-input[type="submit"]'
@@ -98,16 +148,20 @@ async function run(url, type, productDetailInfo, callback) {
       // await page.keyboard.press('Enter')
       // await page.waitForTimeout(2000)
     }
-    await getProductsInfo(page, productDetailInfo)
-    await gotoNextPage(page, productDetailInfo)
-    log(blue(`${type} 获取数据总条数`, productDetailInfo.length))
-    await saveProductInfo(page, type, shuffleSort(productDetailInfo))
-    callback(null, `${type} 获取数据结束`)
+    await getProductsInfo(page, productDetailInfo);
+    await gotoNextPage(page, productDetailInfo);
+    log(blue(`${type} 获取数据总条数`, productDetailInfo.length));
+    if (productDetailInfo.length === 0) {
+      callback(null, `${type} 获取数据结束`);
+      return browser.close();
+    }
+    await saveProductInfo(page, type, shuffleSort(productDetailInfo));
+    callback(null, `${type} 获取数据结束`);
   } catch (err) {
-    callback(err)
-    log(red(err))
+    callback(err);
+    log(red(err));
   } finally {
-    await browser.close()
+    await browser.close();
   }
 }
 
@@ -115,7 +169,7 @@ async function run(url, type, productDetailInfo, callback) {
 // /\/dp\/(.*?)\//i.exec(location.pathname)
 
 async function saveProductInfo(page, type, productDetailInfo) {
-  const productLength = productDetailInfo.length
+  const productLength = productDetailInfo.length;
   for (let i = 0; i < productLength; i++) {
     log(
       blue(
@@ -123,52 +177,54 @@ async function saveProductInfo(page, type, productDetailInfo) {
           ((i + 1) / productLength) * 100
         }%`
       )
-    )
+    );
     try {
-      const productUrl = productDetailInfo[i].link
-      const productId = /\/dp\/(.*?)\//i.exec(productUrl)[1]
-      const time = new Date().format('yyyy-MM-dd').toString()
-      const good = await Goods.findOne({ time, productId })
-      if (good) continue
-      await page.goto(productUrl, waitGotoUrlOption)
+      const productUrl = productDetailInfo[i].link;
+      const productId = /\/dp\/(.*?)\//i.exec(productUrl)[1];
+      const time = new Date().format('yyyy-MM-dd').toString();
+      const good = await Goods.findOne({ time, productId });
+      if (good) continue;
+      await page.goto(productUrl, waitGotoUrlOption);
       const productInfo = await page.evaluate(async () => {
         const sleep = (time = 1000) => {
-          return new Promise(resolve => {
-            setTimeout(() => resolve(), time)
-          })
-        }
+          return new Promise((resolve) => {
+            setTimeout(() => resolve(), time);
+          });
+        };
         for (var y = 0; y <= 2; y++) {
-          window.scrollTo(0, 1280 * y)
-          await sleep()
+          window.scrollTo(0, 1280 * y);
+          await sleep(500);
         }
         var getKeys = {
           rank: '亚马逊热销商品排名',
-          available: 'Amazon.cn上架时间',
-        }
+          available: 'Amazon.cn上架时间'
+        };
         // var getKeys = {
         //   rank: 'Best Sellers Rank',
         //   available: 'Date First Available',
         // }
         let information = {
-          productTitle: document.querySelector('#productTitle')?.innerText,
-        }
+          productTitle: document.querySelector('#productTitle')?.innerText
+        };
         const productDetailTable = document.querySelectorAll(
           '.prodDetSectionEntry'
-        )
+        );
         const productDetailList = document.querySelectorAll(
           '.detail-bullet-list .a-list-item .a-text-bold'
-        )
+        );
         const productDetail =
-          productDetailTable.length > 0 ? productDetailTable : productDetailList
-        productDetail.forEach(item => {
+          productDetailTable.length > 0
+            ? productDetailTable
+            : productDetailList;
+        productDetail.forEach((item) => {
           for (let i in getKeys) {
             if (item.innerText.includes(getKeys[i])) {
-              information[i] = item.nextElementSibling.innerText
+              information[i] = item.nextElementSibling.innerText;
             }
           }
-        })
-        return information
-      })
+        });
+        return information;
+      });
       const goods = new Goods(
         Object.assign(
           {},
@@ -178,32 +234,32 @@ async function saveProductInfo(page, type, productDetailInfo) {
 
           { time, productId }
         )
-      )
-      const result = await goods.save()
-      if (!result) continue
-      await page.waitForTimeout(getRandomInt({ min: 500, max: 1000 }))
+      );
+      const result = await goods.save();
+      if (!result) continue;
+      // await page.waitForTimeout(getRandomInt({ min: 500, max: 1000 }))
     } catch (err) {
-      log(red(err))
-      continue
+      log(red(err));
+      continue;
     }
   }
 }
 
 const start = async () => {
-  log(orange(`start spider`))
+  log(orange(`start spider`));
   _async.mapLimit(
     shuffleSort(categoryTotalInfo),
-    2,
+    4,
     (item, callback) => {
-      run(item.link, item.name, [], callback)
+      run(item.link, item.name, [], callback);
     },
     (err, res) => {
       if (err) {
-        console.log('err======>', err)
+        console.log('err======>', err);
       }
-      console.log('==========>  ', res)
+      console.log('==========>  ', res);
     }
-  )
-}
+  );
+};
 
-start()
+start();
